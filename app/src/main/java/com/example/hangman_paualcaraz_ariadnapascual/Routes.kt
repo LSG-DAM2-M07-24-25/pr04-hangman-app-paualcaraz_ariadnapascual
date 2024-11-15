fabe4650813ef4cfa5a60cd101a0d3f0eebb08e8
pau.alcaraz <paualcarazsanz@gmail.com>
2024-11-15 16:08:35 +0100

    Routes

--- app/src/main/java/com/example/hangman_paualcaraz_ariadnapascual/Routes.kt
+++ app/src/main/java/com/example/hangman_paualcaraz_ariadnapascual/Routes.kt
@@ -1,1 +1,13 @@
+package com.example.navigation
 
+sealed class Routes(val route: String){
+    object MenuScreen : Routes ("MenuScreen")
+    object GameScreen : Routes ("GameScreen/{dificultadSeleccionada}"){
+        fun createRoute(dificultadSeleccionada: String) = "GameScreen/$dificultadSeleccionada"
+    }
+}
+
+object ResultScreen : Routes("ResultScreen/{dificultadSeleccionada}/{estado}/{intentosConsumidos}/{palabraSecreta}"){
+    fun createRoute(diificultadSeleccionada: String, estado: String, intentosConsumidos:Int, palabraSecreta: String) =
+        "ResultScreen /$dificultadSeleccionada/$estado/$intentosConsumidos/$palabraSecreta"
+}
