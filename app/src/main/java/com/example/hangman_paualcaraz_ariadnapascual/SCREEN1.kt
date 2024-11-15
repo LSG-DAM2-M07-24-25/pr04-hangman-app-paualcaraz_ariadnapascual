f94514f8adc9b3d50673f8bd28a924ae055c2932
pau.alcaraz <paualcarazsanz@gmail.com>
2024-11-15 16:20:22 +0100

    update

--- app/src/main/java/com/example/hangman_paualcaraz_ariadnapascual/SCREEN1.kt
+++ app/src/main/java/com/example/hangman_paualcaraz_ariadnapascual/SCREEN1.kt
@@ -1,1 +1,21 @@
+package com.example.hangman_paualcaraz_ariadnapascual
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.hangman_paualcaraz_ariadnapascual.ui.theme.Hangman_PauAlcaraz_AriadnaPascualTheme
+
+
+
+@Composable
+fun pantalla(){
+
+}
