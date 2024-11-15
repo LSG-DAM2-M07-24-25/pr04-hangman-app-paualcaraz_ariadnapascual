88002987cabb8197991cad0316efb40dc2b7898f
ariadna <ari.palau20@gmail.com>
2024-11-15 16:31:23 +0100

    Remove .idea from tracking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.hangman_paualcaraz_ariadnapascual"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.hangman_paualcaraz_ariadnapascual"
