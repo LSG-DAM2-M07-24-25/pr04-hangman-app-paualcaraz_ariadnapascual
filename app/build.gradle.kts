a74bf50c86215e5d8d79836b27755a773731d6ad
ariadna <ari.palau20@gmail.com>
2024-11-15 17:13:21 +0100

    dependencia appcompat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.appcompat:appcompat:1.6.1")
+     // Asegúrate de usar la última versión disponible
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
